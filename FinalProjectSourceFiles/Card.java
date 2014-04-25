7615ee258c84a91be2b306a9b5286d5d06a9cc71
Kyle Kurzhal <kkurzhal@gmail.com>
2014-04-25 11:29:50 -0500

    Updating the coordinates of each card.

--- FinalProjectSourceFiles/Card.java
+++ FinalProjectSourceFiles/Card.java
@@ -4,12 +4,35 @@ public class Card {
 	String suit;
 	String face;
 	int value;
+	private int x;
+	private int y;
+	private final int width = 75;
+	private final int height = 104;
 	
-	Card(String s, String f, int v) {
+	Card(String s, String f, int v, int xVal, int yVal) {
 		
 		suit = s;
 		face = f;
 		value = v;
+		x = xVal;
+		y = yVal;
+	}
+	
+	//Kyle
+	public int getX(){
+		return x;
+	} 
+	
+	public int getY(){
+		return y;
+	}
+	
+	public int getWidth(){
+		return width;
+	}
+	
+	public int getHeight(){
+		return height;
 	}
 	
 	public String getSuit(){
